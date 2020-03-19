40a4648a40d9e7db5dd3c0fcb23c44eac197655e
NicoleAnginoli <nicole_anginoli@hotmail.com>
2020-03-19 09:47:39 -0300

    adicionando estrutura de pastas

---
+++ src/test/java/gerenciamentopontoeletronico/GerenciamentoPontoEletronicoItauEstagsApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamentopontoeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPontoEletronicoItauEstagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
