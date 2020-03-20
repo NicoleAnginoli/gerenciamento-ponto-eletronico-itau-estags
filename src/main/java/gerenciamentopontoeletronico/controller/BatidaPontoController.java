4500c4e15f35ecc0abf901944dbf2eb07939d025
NicoleAnginoli <nicole_anginoli@hotmail.com>
2020-03-20 09:12:08 -0300

    criacao dos metodos de get e post dos pontos eletronicos

--- src/main/java/gerenciamentopontoeletronico/controller/BatidaPontoController.java
+++ src/main/java/gerenciamentopontoeletronico/controller/BatidaPontoController.java
@@ -1,28 +1,34 @@
 package gerenciamentopontoeletronico.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import gerenciamentopontoeletronico.model.BatidaPonto;
+import gerenciamentopontoeletronico.model.Funcionario;
 import gerenciamentopontoeletronico.repository.BatidaPontoRepository;
 
 @RestController
+@RequestMapping(value="/api/ponto")
 public class BatidaPontoController {
 	@Autowired
 	BatidaPontoRepository batidaPontoRepository;
 	
-	@GetMapping("/api/ponto")
+	@GetMapping()
 	public List<BatidaPonto> getAllRegistries(){
 		return batidaPontoRepository.findAll();
 	}
 	
-	@PostMapping("/funcionarios/inserir")
-	public BatidaPonto insertEmployee(@RequestBody BatidaPonto batidaPonto) {
+	@PostMapping("/inserir")
+	public BatidaPonto registryEmployeeTime(@RequestBody BatidaPonto batidaPonto) {
 		return batidaPontoRepository.save(batidaPonto);
 	}
 }
