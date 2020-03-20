d93c4b0fa29ac0309cac3914bdf7cd0a19656a15
NicoleAnginoli <nicole_anginoli@hotmail.com>
2020-03-20 11:22:10 -0300

    condicional para barrar duas batidas do mesmo tipo, falta mensagem erro

--- src/main/java/gerenciamentopontoeletronico/controller/BatidaPontoController.java
+++ src/main/java/gerenciamentopontoeletronico/controller/BatidaPontoController.java
@@ -3,6 +3,7 @@ package gerenciamentopontoeletronico.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,12 +27,26 @@ public class BatidaPontoController {
 	}
 	
 	@PostMapping("/inserir")
-	public BatidaPonto registryEmployeeTime(@RequestBody BatidaPonto batidaPonto) {
-		return batidaPontoRepository.save(batidaPonto);
+	public ResponseEntity<BatidaPonto> registryEmployeeTime(@RequestBody BatidaPonto batidaPonto) {
+		boolean valid = isRegistryValid(batidaPonto.getidfunc(), batidaPonto.gettipobatida());
+		if (valid) {
+			BatidaPonto inserido = batidaPontoRepository.save(batidaPonto);
+			return ResponseEntity.ok().body(inserido);
+		}
+		return ResponseEntity.badRequest().build();
 	}
 	
 	@GetMapping("/{idfunc}")
 	public @ResponseBody List<BatidaPonto> getEmployeeRegistries(@PathVariable(value="idfunc") int idfunc){
 		return batidaPontoRepository.findByIdfunc(idfunc);
+	}
+	
+	@GetMapping("/teste")
+	public boolean isRegistryValid(int idfunc, String tipo) {
+		String ultimaentrada = batidaPontoRepository.checkRegistryType(idfunc);
+		if (ultimaentrada.equals(tipo)) {
+			return false;
+		}
+		return true;
 	}
 }
--- src/main/java/gerenciamentopontoeletronico/repository/BatidaPontoRepository.java
+++ src/main/java/gerenciamentopontoeletronico/repository/BatidaPontoRepository.java
@@ -3,6 +3,7 @@ package gerenciamentopontoeletronico.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import gerenciamentopontoeletronico.model.BatidaPonto;
@@ -10,4 +11,7 @@ import gerenciamentopontoeletronico.model.BatidaPonto;
 @Repository
 public interface BatidaPontoRepository extends JpaRepository <BatidaPonto, Integer>{
 	List<BatidaPonto> findByIdfunc (int idfunc);
+	
+	@Query(value="SELECT tipobatida FROM batidaponto WHERE id = (SELECT MAX(id) FROM batidaPonto WHERE idfunc = ?1)", nativeQuery = true)
+	String checkRegistryType(int idfunc);
 }
