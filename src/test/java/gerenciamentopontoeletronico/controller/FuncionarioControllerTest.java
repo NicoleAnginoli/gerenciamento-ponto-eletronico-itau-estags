726b7ac32b3acd7e13116ef4d3408f5043829dc9
NicoleAnginoli <nicole_anginoli@hotmail.com>
2020-03-23 09:36:59 -0300

    criacao classe teste

--- src/test/java/gerenciamentopontoeletronico/controller/FuncionarioControllerTest.java
+++ src/test/java/gerenciamentopontoeletronico/controller/FuncionarioControllerTest.java
@@ -1,5 +1,21 @@
 package gerenciamentopontoeletronico.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//
+//import gerenciamentopontoeletronico.repository.FuncionarioRepository;
 
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class FuncionarioControllerTest {
-
+	
+//	@Autowired
+//	FuncionarioController funcionarioController;
+//	
+//	@MockBean
+//	FuncionarioRepository funcionarioRepository;
+//	
+//	public void getAllEmployeesTest(){
+////		when(funcionarioRepository.findAll()).thenReturn;
+//	}
 }
