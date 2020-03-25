5e2c49a4b6b67a1d186a89ba9392cb057e40c652
NicoleAnginoli <nicole_anginoli@hotmail.com>
2020-03-25 09:47:41 -0300

    mudança no retorno do metodo findById

--- src/main/java/gerenciamentopontoeletronico/repository/FuncionarioRepository.java
+++ src/main/java/gerenciamentopontoeletronico/repository/FuncionarioRepository.java
@@ -1,5 +1,7 @@
 package gerenciamentopontoeletronico.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import gerenciamentopontoeletronico.model.Funcionario;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository <Funcionario, Integer>{
-	 
+//	List<Funcionario> findById(int id);
 }
